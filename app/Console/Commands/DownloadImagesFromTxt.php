5600f5e971f7bd69a08c1ad4f4b596e31c79a295
DeSKot <sgcore2015@gmail.com>
2021-08-18 12:00:30 +0300

    first commit

---
+++ app/Console/Commands/DownloadImagesFromTxt.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class DownloadImagesFromTxt extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'download:txt  {--file=} {--name=} {--d|directory=}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description =  'Command for download images from txt file with URL. Exemple : "php artisan download:txt --file=storage/URI.txt --name=logo --d" 
+    Where "storage/app" is path to directory in local project and "Logo" is new name of download images';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $directory = $this->option('directory') ?? "./";
+        $name = $this->option('name');
+        $file = $this->option('file');
+
+
+        $file = file_get_contents("./$file", true);    //path for file where we have all URL links in txt//                 exemple --f=storage/URL.txt
+        $url_array = explode(' ', $file);              // array with all Links
+
+        $count = 1;
+        foreach ($url_array as $value) {
+            $img_url = $value;
+            $Headers = @get_headers($img_url);
+            if (preg_match("|200|", $Headers[0])) {
+                $image = file_get_contents($img_url);
+                file_put_contents("./$directory/$name$count.jpg", $image);   //Path for uploaded images  and you can change format to another
+                $this->info("$count - count of images");
+                $count++;
+            } else {
+                echo "Not Found";
+            }
+        }
+
+        $this->info('Pictures from the links have been downloaded successfully');
+    }
+}
